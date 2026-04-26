0f2261e28a93cf4f2a94f2b8e6ce21ae9699933e
NFrost <nicolas18sr@gmail.com>
2026-04-26 16:44:20 -0300

    chore: update eslint rules
    modify eslint configuration to work with typescript and add rules I find comfortable

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,26 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+        '@react-native',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    rules: {
+        indent: ["error", 4, {
+            SwitchCase: 1,
+        }],
+
+        semi: ["error", "always"],
+
+        "@typescript-eslint/no-unused-vars": ["warn", {
+            argsIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+        }],
+
+        "@typescript-eslint/no-require-imports": "off",
+
+        "prefer-const": "warn",
+        "no-empty": "off",
+    }
 };
