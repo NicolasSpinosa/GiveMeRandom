f0f3ec6574ed584867d3995787fd18b233bd8307
NFrost <nicolas18sr@gmail.com>
2026-04-27 15:53:29 -0300

    feat: wrap app content into safe area view

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { AppContent } from "./src/components/structure/AppContent";
 
 const App = () => (
     <SafeAreaProvider>
-
+        <AppContent/>
     </SafeAreaProvider>
 );
 
