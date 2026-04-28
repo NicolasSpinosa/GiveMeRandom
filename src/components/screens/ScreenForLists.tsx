bd02097419b7f874099db5886ecfc74c91490ca6
NFrost <nicolas18sr@gmail.com>
2026-04-28 16:20:24 -0300

    feat: create very basic structure for all screens

---
+++ src/components/screens/ScreenForLists.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+import { structureStyles } from "../../styles/structureStyles";
+import { Text } from "../generics/Text";
+
+export const ScreenForLists = () => (
+    <View style={structureStyles.screen}>
+        <Text>Lists</Text>
+    </View>
+);
---
+++ src/components/screens/ScreenForNumbers.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+import { structureStyles } from "../../styles/structureStyles";
+import { Text } from "../generics/Text";
+
+export const ScreenForNumbers = () => (
+    <View style={structureStyles.screen}>
+        <Text>Numbers</Text>
+    </View>
+);
