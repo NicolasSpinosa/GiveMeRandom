42071761a8bb17d601c42e9a3e7edd2129c5ceb1
NFrost <nicolas18sr@gmail.com>
2026-04-28 16:20:55 -0300

    feat: update navigator to include new screen components and update the name of "Premade" screen to "Named"

--- src/components/structure/AppNavigator.tsx
+++ src/components/structure/AppNavigator.tsx
@@ -1,14 +1,17 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
+import { ScreenForNumbers } from "../screens/ScreenForNumbers";
+import { ScreenForLists } from "../screens/ScreenForLists";
+import { ScreenForNamed } from "../screens/ScreenForNamed";
 
 const Drawer = createDrawerNavigator();
 
 export const AppNavigator = () => (
     <NavigationContainer>
         <Drawer.Navigator>
-            <Drawer.Screen name="Numbers" component={() => <></>}/>
-            <Drawer.Screen name="Lists" component={() => <></>}/>
-            <Drawer.Screen name="Premade" component={() => <></>}/>
+            <Drawer.Screen name="Numbers" component={ScreenForNumbers}/>
+            <Drawer.Screen name="Lists" component={ScreenForLists}/>
+            <Drawer.Screen name="Named" component={ScreenForNamed}/>
         </Drawer.Navigator>
     </NavigationContainer>
 );
