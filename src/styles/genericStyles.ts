a5e65059de56b3d342cf446a55c67a18c5540f4a
NFrost <nicolas18sr@gmail.com>
2026-04-28 16:18:55 -0300

    feat: created generic style sheet
    this style sheet is used for custom, reusable components

---
+++ src/styles/genericStyles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const genericStyles = StyleSheet.create({
+    text: {
+        includeFontPadding: false,
+        textAlignVertical:"center"
+    }
+});
