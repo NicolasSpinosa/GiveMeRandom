d1df9d176f4bf2b37e878ebaddfbe421ea92e4bb
NFrost <nicolas18sr@gmail.com>
2026-04-30 16:48:44 -0300

    feat: add the custom header to the app's navigator

--- src/components/structure/AppNavigator.tsx
+++ src/components/structure/AppNavigator.tsx
@@ -1,14 +1,20 @@
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createDrawerNavigator, DrawerHeaderProps } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { ScreenForNumbers } from "../screens/ScreenForNumbers";
 import { ScreenForLists } from "../screens/ScreenForLists";
 import { ScreenForNamed } from "../screens/ScreenForNamed";
+import { AppHeader } from "./AppHeader";
 
 const Drawer = createDrawerNavigator();
+const appHeader = (props: DrawerHeaderProps) => <AppHeader {...props}/>;
 
 export const AppNavigator = () => (
     <NavigationContainer>
-        <Drawer.Navigator>
+        <Drawer.Navigator
+            screenOptions={{
+                header: appHeader
+            }}
+        >
             <Drawer.Screen name="Numbers" component={ScreenForNumbers}/>
             <Drawer.Screen name="Lists" component={ScreenForLists}/>
             <Drawer.Screen name="Named" component={ScreenForNamed}/>
