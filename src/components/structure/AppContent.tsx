e63319f0838ab2db216f43c5a77aa90ceebd0330
NFrost <nicolas18sr@gmail.com>
2026-04-30 11:44:12 -0300

    feat: add navigator to render on app content

--- src/components/structure/AppContent.tsx
+++ src/components/structure/AppContent.tsx
@@ -1,8 +1,9 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { structureStyles } from "../../styles/structureStyles";
+import { AppNavigator } from "./AppNavigator";
 
 export const AppContent = () => (
     <SafeAreaView style={structureStyles.background}>
-        
+        <AppNavigator/>
     </SafeAreaView>
 );
