6c00a1bdfc4921aafe18e5638e1baf7617a4efff
NFrost <nicolas18sr@gmail.com>
2026-05-02 15:43:58 -0300

    feat: very small scaffold of how the number generation should look like

--- src/components/screens/ScreenForNumbers.tsx
+++ src/components/screens/ScreenForNumbers.tsx
@@ -1,9 +1,34 @@
-import { View } from "react-native";
+import { TextInput, View } from "react-native";
 import { appStyles } from "../../styles/appStyles";
 import { Text } from "../generics/Text";
 
 export const ScreenForNumbers = () => (
     <View style={appStyles.screen}>
-        <Text>Numbers</Text>
+        <View style={{flexDirection: "row", gap: 8}}>
+            <Text>Min:</Text>
+            <TextInput
+                keyboardType="numeric"
+                value="0"
+                style={{
+                    backgroundColor:"red",
+                    flex: 1,
+                    borderRadius: 10,
+                    borderWidth: 2
+                }}
+            />
+        </View>
+        <View style={{flexDirection: "row", gap: 8}}>
+            <Text>Max:</Text>
+            <TextInput
+                keyboardType="numeric"
+                value="0"
+                style={{
+                    backgroundColor:"red",
+                    flex: 1,
+                    borderRadius: 10,
+                    borderWidth: 2
+                }}
+            />
+        </View>
     </View>
 );
