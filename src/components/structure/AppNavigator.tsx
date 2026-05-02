6e56dedd3a5ec7c8939a6db173136289419c35a3
NFrost <nicolas18sr@gmail.com>
2026-05-02 16:34:35 -0300

    feat: experiment with drawer styles

--- src/components/structure/AppNavigator.tsx
+++ src/components/structure/AppNavigator.tsx
@@ -1,7 +1,8 @@
-import { createDrawerNavigator, DrawerHeaderProps } from "@react-navigation/drawer";
+import { createDrawerNavigator, DrawerContentComponentProps, DrawerContentScrollView, DrawerHeaderProps, DrawerItem, DrawerItemList } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { ScreenForNumberRange } from "../screens/ScreenForNumberRange";
 import { AppHeader } from "./AppHeader";
+import { View } from "react-native";
 
 const Drawer = createDrawerNavigator();
 const appHeader = (props: DrawerHeaderProps) => <AppHeader {...props}/>;
@@ -10,7 +11,13 @@ export const AppNavigator = () => (
     <NavigationContainer>
         <Drawer.Navigator
             screenOptions={{
-                header: appHeader
+                header: appHeader,
+                drawerStyle: {backgroundColor: "none"},
+                drawerContentStyle: {backgroundColor: "white"},
+                drawerItemStyle: {
+                    borderRadius: 8,
+                },
+                drawerLabelStyle: {fontSize: 18}
             }}
         >
             <Drawer.Screen name="Number range" component={ScreenForNumberRange}/>
