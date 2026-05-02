3a3367421d4d43656c7a0eccb53551c46ae52d0c
NFrost <nicolas18sr@gmail.com>
2026-05-02 17:36:01 -0300

    feat: parametrized settings as their own component

--- src/components/screens/ScreenForSettings.tsx
+++ src/components/screens/ScreenForSettings.tsx
@@ -1,16 +1,33 @@
-import { View } from "react-native";
+import { TextProps, View } from "react-native";
 import { appStyles } from "../../styles/appStyles";
 import { Text } from "../generics/Text";
 
+type SettingProps = {
+    label: string,
+    labelProps?: TextProps,
+    Component: React.FC
+}
+
+const Setting = ({
+    label,
+    labelProps,
+    Component
+}: SettingProps) => (
+    <View style={{flexDirection: "row", justifyContent: "space-between"}}>
+        <Text {...labelProps}>{label}</Text>
+        <Component/>
+    </View>
+);
+
 export const ScreenForSettings = () => (
     <View style={appStyles.screen}>
-        <View style={{flexDirection: "row", gap: 8}}>
-            <Text>Theme</Text>
-            <Text>System</Text>
-        </View>
-        <View style={{flexDirection: "row", gap: 8}}>
-            <Text>Language</Text>
-            <Text>English</Text>
-        </View>
+        <Setting
+            label="Theme"
+            Component={() => <Text>System</Text>}
+        />
+        <Setting
+            label="Language"
+            Component={() => <Text>English</Text>}
+        />
     </View>
 );
