3bd9e3b21a6469b8ba424b1b11fa3ee4105becc3
NFrost <nicolas18sr@gmail.com>
2026-05-02 13:30:25 -0300

    feat: app header button now uses the options icon instead of text

--- src/components/structure/AppHeaderButton.tsx
+++ src/components/structure/AppHeaderButton.tsx
@@ -1,8 +1,9 @@
 import { Pressable, PressableProps } from "react-native";
 import { Text } from "../generics/Text";
+import OptionsIcon from "../../assets/icons/options.svg";
 
 export const AppHederButton = (props: PressableProps) => (
     <Pressable {...props}>
-        <Text>DRAWER</Text>
+        <OptionsIcon width={16} height={16}/>
     </Pressable>
 );
