415bd973c461b1875f759078cfbd96952d7e6785
NFrost <nicolas18sr@gmail.com>
2026-05-04 17:26:46 -0300

    fix: wrapped theme setting's text with a Text component

--- src/components/settings/SettingForTheme.tsx
+++ src/components/settings/SettingForTheme.tsx
@@ -1,12 +1,13 @@
 import { Pressable } from "react-native";
 import { Setting } from "./Setting";
+import { Text } from "../generics/Text";
 
 export const SettingForTheme = () => (
     <Setting
         label="Theme"
         setting={
             <Pressable>
-                Hello
+                <Text>Hello</Text>
             </Pressable>
         }
     />
