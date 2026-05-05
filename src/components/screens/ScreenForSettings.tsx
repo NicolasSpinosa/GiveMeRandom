a8821ec52c1f1cb370a6fae76af662ba2cac15cc
NFrost <nicolas18sr@gmail.com>
2026-05-05 15:35:20 -0300

    feat: Removed preliminary settings from their screen.
    Done as the setting component is about to be reworked.

--- src/components/screens/ScreenForSettings.tsx
+++ src/components/screens/ScreenForSettings.tsx
@@ -1,15 +1,7 @@
 import { View } from "react-native";
 import { appStyles } from "../../styles/appStyles";
-import { Text } from "../generics/Text";
-import { Setting } from "../settings/Setting";
-import { SettingForTheme } from "../settings/SettingForTheme";
 
 export const ScreenForSettings = () => (
     <View style={appStyles.screen}>
-        <SettingForTheme/>
-        <Setting
-            label="Language"
-            setting={<Text>English</Text>}
-        />
     </View>
 );
