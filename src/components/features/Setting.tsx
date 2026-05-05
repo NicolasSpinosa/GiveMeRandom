a554391fc8e1ed2e42ecd4ad63627bf882caf4cc
NFrost <nicolas18sr@gmail.com>
2026-05-05 16:57:39 -0300

    feat: Coupled the setting component to open a dialog box.
    This is done because, as of now, all a setting component should do is open a dialog box.

--- src/components/features/Setting.tsx
+++ src/components/features/Setting.tsx
@@ -1,26 +1,42 @@
 import { GestureResponderEvent, Pressable, View } from "react-native";
 import { Text } from "../primitive/Text";
 import { settingStyles } from "../../styles/setting";
+import { useState } from "react";
+import { DialogBox } from "./DialogBox";
 
 type SettingProps = {
     label: string,
     description?: string,
     onPress?: (event: GestureResponderEvent) => void,
-    decorator?: React.ReactNode
+    decorator: React.ReactNode,
+    dialogContent: React.ReactNode
 }
 
 export const Setting = ({
     label,
     description,
-    decorator
-}: SettingProps) => (
-    <Pressable
-        style={settingStyles.pressable}
-    >
-        <View style={settingStyles.descriptor}>
-            <Text>{label}</Text>
-            {description && <Text>{description}</Text>}
-        </View>
-        <View>{decorator}</View>
-    </Pressable>
-);
+    decorator,
+    dialogContent
+}: SettingProps) => {
+    const [visible, setVisible] = useState(false);
+
+    return (
+        <>
+            <Pressable
+                style={settingStyles.pressable}
+            >
+                <View style={settingStyles.descriptor}>
+                    <Text>{label}</Text>
+                    {description && <Text>{description}</Text>}
+                </View>
+                {decorator && <View>{decorator}</View>}
+            </Pressable>
+            <DialogBox
+                visible={visible}
+                setVisible={setVisible}
+            >
+                {dialogContent}
+            </DialogBox>
+        </>
+    );
+};
