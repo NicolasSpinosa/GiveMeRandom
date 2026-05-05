0e7f524792172f9432ee2509377c0ad9fafa836b
NFrost <nicolas18sr@gmail.com>
2026-05-05 14:11:10 -0300

    build: removed @gorhom/bottom-sheet, as it was overkill for the app's purpose.

--- src/components/structure/AppContent.tsx
+++ src/components/structure/AppContent.tsx
@@ -1,12 +1,9 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { appStyles } from "../../styles/appStyles";
 import { AppNavigator } from "./AppNavigator";
-import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 
 export const AppContent = () => (
     <SafeAreaView style={appStyles.background}>
-        <BottomSheetModalProvider>
-            <AppNavigator/>
-        </BottomSheetModalProvider>
+        <AppNavigator/>
     </SafeAreaView>
 );
