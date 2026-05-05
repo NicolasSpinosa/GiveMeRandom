567b116df814b57193ca0ec7685a4829cb72b9bc
NFrost <nicolas18sr@gmail.com>
2026-05-05 16:08:46 -0300

    chore: Moved the header button back to header file as it didn't deserve its own file.

--- src/components/structure/AppHeader.tsx
+++ src/components/structure/AppHeader.tsx
@@ -1,8 +1,8 @@
 import { DrawerHeaderProps } from "@react-navigation/drawer";
-import { View } from "react-native";
+import { Pressable, View } from "react-native";
 import { Text } from "../primitive/Text";
 import { appStyles } from "../../styles/appStyles";
-import { AppHederButton } from "./AppHeaderButton";
+import OptionsIcon from "../../assets/icons/options.svg";
 
 export const AppHeader = ({
     layout,
@@ -14,7 +14,9 @@ export const AppHeader = ({
 
     return (
         <View style={appStyles.header}>
-            <AppHederButton onPress={navigation.openDrawer}/>
+            <Pressable onPress={navigation.openDrawer}>
+                <OptionsIcon width={18} height={18} />
+            </Pressable>
             <Text style={appStyles.headerTitle}>{title}</Text>
         </View>
     );
--- src/components/structure/AppHeaderButton.tsx
+++
@@ -1,8 +0,0 @@
-import { Pressable, PressableProps } from "react-native";
-import OptionsIcon from "../../assets/icons/options.svg";
-
-export const AppHederButton = (props: PressableProps) => (
-    <Pressable {...props}>
-        <OptionsIcon width={18} height={18} />
-    </Pressable>
-);
