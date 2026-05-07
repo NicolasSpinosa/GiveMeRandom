58a184101777759ac2ec52524921527b66d6719f
NFrost <nicolas18sr@gmail.com>
2026-05-07 11:52:09 -0300

    chore: Created utility file to hold common utility functions for components.

---
+++ src/components/utils.ts
@@ -0,0 +1,6 @@
+import { ComponentWithProps } from "../types";
+
+export const packProviderWithProps = <P extends object = object>(
+    provider: React.ComponentType<P>,
+    props?: P
+): ComponentWithProps<P> => [provider, props];
