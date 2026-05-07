2edf7513b301feffb4bb11a96287202a288112b4
NFrost <nicolas18sr@gmail.com>
2026-05-07 12:50:58 -0300

    chore: Renamed structure folder to navigation.

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { ComposeProviders } from "./src/components/providers/ComposeProviders";
 import { packProviderWithProps } from "./src/utils";
 import { PaperProvider } from "react-native-paper";
-import { RootNavigator } from "./src/components/structure/RootNavigator";
+import { RootNavigator } from "./src/components/navigation/RootNavigator";
 
 const App = () => (
     <ComposeProviders packedProviders={[
